768284bd6119940f890cbe2b6667df77e588ab08
rutger <rutgercappendijk@gmail.com>
2024-09-06 15:26:32 +0200

    updated some pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,11 @@
-<script>
+<script lang="ts">
 	import '../app.css';
-	import { Heart, Icon, Bars3 } from 'svelte-hero-icons';
+	import { Heart, Icon, XMark, Bars3 } from 'svelte-hero-icons';
 
 	let menuOpen = false;
 
 	function toggleMenu() {
+		console.log("menu" + menuOpen)
 		menuOpen = !menuOpen;
 	}
 </script>
@@ -13,7 +14,6 @@
 	<div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
 		<div class="relative flex h-16 justify-between">
 			<div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
-				<!-- Mobile menu button -->
 				<button
 					type="button"
 					on:click={toggleMenu}
@@ -23,33 +23,14 @@
 				>
 					<span class="absolute -inset-0.5"></span>
 					<span class="sr-only">Open main menu</span>
-					<!--
-			  Icon when menu is closed.
-  
-			  Menu open: "hidden", Menu closed: "block"
-			-->
 					<Icon src={Bars3} class=" {menuOpen ? 'hidden' : 'block'} block h-6 w-6" />
-					<!--
-			  Icon when menu is open.
-  
-			  Menu open: "block", Menu closed: "hidden"
-			-->
-					<svg
-						class="{menuOpen ? 'block' : 'hidden'} h-6 w-6"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke-width="1.5"
-						stroke="currentColor"
-						aria-hidden="true"
-					>
-						<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-					</svg>
+					<Icon src={XMark} class=" {menuOpen ? 'block' : 'hidden'} block h-6 w-6" />
 				</button>
 			</div>
 			<div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-				<div class="flex flex-shrink-0 items-center">
+				<a href="/" class="flex flex-shrink-0 items-center">
 					<Icon src={Heart} class="h-8 w-auto" />
-				</div>
+				</a>
 				<div class="hidden sm:ml-6 sm:flex sm:space-x-8">
 					<!-- Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" -->
 					<a
@@ -118,40 +99,13 @@
 							/>
 						</button>
 					</div>
-
-					<!--
-			  Dropdown menu, show/hide based on menu state.
-  
-			  Entering: "transition ease-out duration-200"
-				From: "transform opacity-0 scale-95"
-				To: "transform opacity-100 scale-100"
-			  Leaving: "transition ease-in duration-75"
-				From: "transform opacity-100 scale-100"
-				To: "transform opacity-0 scale-95"
-			-->
-					<div
-						class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-						role="menu"
-						aria-orientation="vertical"
-						aria-labelledby="user-menu-button"
-						tabindex="-1"
-					>
-						<!-- Active: "bg-gray-100", Not Active: "" -->
-						<!-- <a
-							href="#"
-							class="block px-4 py-2 text-sm text-gray-700"
-							role="menuitem"
-							tabindex="-1"
-							id="user-menu-item-0">Your Profile</a
-						> -->
-					</div>
 				</div>
 			</div>
 		</div>
 	</div>
 
 	<!-- Mobile menu, show/hide based on menu state. -->
-	<div class="sm:hidden" id="mobile-menu">
+	<div class="sm:hidden {menuOpen ? '' : 'hidden'}" id="mobile-menu">
 		<div class="space-y-1 pb-4 pt-2">
 			<!-- Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" -->
 			<a
