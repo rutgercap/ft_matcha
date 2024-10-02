e27b0b305e9d387777b5ae6b9ea4ce7d1398f209
rutger <rutgercappendijk@gmail.com>
2024-10-02 16:28:24 +0200

    lint

--- src/routes/(authenticated)/+layout.server.ts
+++ src/routes/(authenticated)/+layout.server.ts
@@ -6,7 +6,7 @@ export const load: LayoutServerLoad = async ({ url, locals: { user } }) => {
 	if (!user && !url.pathname.startsWith('/sign-in')) {
 		redirect(302, '/sign-in');
 	}
-	let currentUser = user as User;
+	const currentUser = user as User;
 	if (!currentUser.profileIsSetup && !url.pathname.startsWith('/profile/edit-personal-info')) {
 		redirect(302, '/profile/edit-personal-info');
 	}
