4883b56ff9c0f2701ff679984602ede86d993a7c
rutger <rutgercappendijk@gmail.com>
2024-11-22 14:02:05 +0100

    fixed match page

--- src/routes/(authenticated)/matches/+page.server.ts
+++ src/routes/(authenticated)/matches/+page.server.ts
@@ -9,9 +9,16 @@ export const load: PageServerLoad = async ({
 		throw redirect(401, '/login');
 	}
 	const matches = await connectionRepository.matchesForUser(user.id);
-	const profilePreviews = await userRepository.profilePreviews(matches.map((match) => match.userTwo));
+	const matchPreviews = await userRepository.profilePreviews(matches.map((match) => match.userTwo));
+	let likes = await connectionRepository.likes(user.id);
+	likes = likes.filter((like) => !matches.some((match) => match.userTwo === like));
+	const likedProfilePreviews = await userRepository.profilePreviews(likes);
+	let likedBy = await connectionRepository.userLikedBy(user.id);
+	likedBy = likedBy.filter((like) => !matches.some((match) => match.userTwo === like));
+	const likedByProfilePreviews = await userRepository.profilePreviews(likedBy);
 	return {
-		matches,
-		profilePreviews
+		matchPreviews,
+		likedProfilePreviews,
+		likedByProfilePreviews,
 	};
 };
